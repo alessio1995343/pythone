45db75f47bf3caa08846b538ff20eb5b9ea20f64
alessio <alessiostomeo04@gmail.com>
2025-03-18 12:57:08 +0100

    funzione somma

---
+++ somma.py
@@ -0,0 +1,6 @@
+valori_1 = input("Passami il primo valore: ")
+valori_2 = input("Passami il secondo valore: ")
+v1 = int(valori_1)
+v2 = int(valori_2)
+somma = v1 + v2
+print("La somma dei due valori è:", somma)
