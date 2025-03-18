b1c618b374c13b147172ed1ab91b348d47dc4c0d
alessio <alessiostomeo04@gmail.com>
2025-03-18 16:01:05 +0100

    programma mesi

---
+++ mesi.py
@@ -0,0 +1,8 @@
+
+mese = "gennaio  " + "febbraio "+ "marzo    "+ "aprile   "+ "maggio   "+ "giugno   "+ "luglio   "+ "agosto   "+ "settembre"+ "ottobre  " +"novembre "+ "dicembre "
+mesi = int(input("Indica mese: "))
+p =(mesi - 1) * 9
+print (mese[p],mese[p+1],mese[p+2],mese[p+3],mese[p+4],mese[p+5],mese[p+6],mese[p+7],mese[p+8])
+mesi = int(input("Indica mese: "))
+#p =(mesi - 1) * 9
+#print(mese[p:p+9])
