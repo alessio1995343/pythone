556724d14a7eceaf1c7e60c6a9c603b37d1b829e
alessio <alessiostomeo04@gmail.com>
2025-03-18 12:41:26 +0100

    round

--- saluto.py
+++ saluto.py
@@ -1,3 +1,11 @@
-nome = "Alessio Stomeo"
-print("Mi chiamo", nome)
+#nome = "Alessio Stomeo"
+#print("Mi chiamo", nome)
+#print(type(nome))
+#nome = 5
+#print(type(nome))
 
+#from math import sqrt, sin, cos
+#print (sqrt(5))
+#print (sqrt(cos(0)**2 + sin(0)**2))
+#print (cos(0), sin(0))
+round(3.1415926535, 2)
