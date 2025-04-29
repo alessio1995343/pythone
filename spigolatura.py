a878cbc7d7c5f29baec30f92cee349b0c5eaa262
alessio1995343 <alessiostomeo04@gmail.com>
2025-04-29 11:12:07 +0200

    Update spigolatura.py

--- spigolatura.py
+++ spigolatura.py
@@ -6,3 +6,11 @@ text = theBytes.decode()
 import bs4 
 doc = bs4.BeautifulSoup(text)
 print (doc.prettify())
+
+def naviga2 (tag,indent):
+    print (indent +tag.name.upper())
+    for stag in tag.contents:
+        if type(stag) == bs4.element.Tag:
+            naviga2 (stag,indent + "  ")
+
+naviga2(doc, "  ") 
